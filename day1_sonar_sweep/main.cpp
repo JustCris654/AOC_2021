588390a48205efa0a8e957457f1a58afed4af4ec
JustCris <cristian.scapin654@icloud.com>
2021-12-11 12:06:02 +0100

    day1 problem 2 solfed

--- day1_sonar_sweep/main.cpp
+++ day1_sonar_sweep/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+void problem_1() {
     ifstream file;
     file.open("input.txt", ios::in);
 
@@ -23,7 +23,54 @@ int main(int argc, char *argv[]) {
     } else {
         cout << "Could not open file.\n";
     }
+}
+
+void problem_2() {
+    ifstream file;
+    file.open("input_2.txt", ios::in);
 
 
+    if (file.is_open()) {
+        int count = std::count(std::istreambuf_iterator<char>(file),
+                std::istreambuf_iterator<char>(), '\n');
+        file.clear();
+        file.seekg(0);
+
+        char input[20];
+        int cur_dep = -1;
+        int index = 0;
+        int arr[count];
+        while (file >> input){
+            cur_dep = atoi(input);
+            arr[index] = cur_dep;
+            index ++;
+        }
+
+        int sum = -1;
+        int prev_sum = -1;
+        int inc_count = 0;
+        for (int i = 0; i < index - 2; ++i){
+            sum = 0;
+            for (int j = 0; j < 3; ++j){
+                sum += arr[i+j];
+            }
+            if (prev_sum != -1 && prev_sum < sum){
+                inc_count++;
+            }
+            prev_sum = sum;
+        }
+
+        cout << inc_count << '\n';
+
+
+
+    } else {
+        cout << "Could not open file.\n";
+    }
+}
+
+int main(int argc, char** argv){
+    // problem_1();
+    problem_2();
     return 0;
 }
